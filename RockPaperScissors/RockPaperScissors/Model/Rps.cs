fefee08c1f4060d94ceec9f8b2b81d27010a52de
Michaela Hübnerová <u18.412@pslib.cloud>
2020-03-06 09:39:42 +0100

    #1

---
+++ RockPaperScissors/RockPaperScissors/Model/Rps.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors.Model
+{
+    public class Rps
+    {
+        public Rps(int winCount = 0, int lossCount = 0, int drawCount = 0)
+        {
+            WinCount = winCount;
+            LossCount = lossCount;
+            DrawCount = drawCount;
+            IsWinner = null;
+        }
+
+        public bool? IsWinner { get; set; }
+        public int WinCount { get; set; }
+        public int LossCount { get; set; }
+        public int DrawCount { get; set; }
+
+    }
+}
---
+++ RockPaperScissors/RockPaperScissors/Services/RpsLogic.cs
@@ -0,0 +1,59 @@
+﻿using RockPaperScissors.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors.Services
+{
+    public class RpsLogic
+    {
+
+        private readonly Random random;
+
+        public RpsLogic(Random random)
+        {
+            this.random = random;
+        }
+
+        public Rps userData { get; set; }
+
+        public void GameRound(RpsMode userChoice)
+        {
+            RpsMode aiChoice = (RpsMode)random.Next(1, 4);
+
+            if (aiChoice == RpsMode.Rock && userChoice == RpsMode.Scissors ||
+                aiChoice == RpsMode.Paper && userChoice == RpsMode.Rock ||
+                aiChoice == RpsMode.Scissors && userChoice == RpsMode.Paper)
+            {
+                userData.LossCount++;
+                userData.IsWinner = false;
+            }
+
+            else if (aiChoice == userChoice)
+            {
+                userData.DrawCount++;
+                userData.IsWinner = null;
+
+            }
+
+            else
+            {
+                userData.WinCount++;
+                userData.IsWinner = true;
+
+            }
+                
+                
+                
+            
+
+
+
+
+
+
+
+        }
+    }
+}
